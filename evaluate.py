8f33c2ac4458c7c4a4082eeba957c93fab3b8f94
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2021-01-11 14:37:10 -0500

    Refining how the zoom-in process works

--- evaluate.py
+++ evaluate.py
@@ -9,6 +9,7 @@ import os
 from random import random
 import argparse
 from distutils.dir_util import copy_tree
+import time
 
 
 
@@ -18,20 +19,25 @@ def main():
     #Clone our database to a version we can tinker with
     copy_tree(args.input, args.tmp_folder)
     #Define the image loader to read in the images from our file
-    for _ in range(5):#TODO set a proper number of itterations
+    for i in range(5):#TODO set a proper number of itterations
         itterate(model)
+        print(str(i) + ': ' + str(times))
     pass
 
 def itterate(model):
     img, _, files = get_data()
     #Generate a set of heatmaps for the given data
+    times['heatmap'] = time.process_time()
     out = model.predict(img)
+    times['heatmap'] = time.process_time() - times['heatmap']
     for i in range(len(out)):
         #As a temporary measure
         mask_image = Image.fromarray((out[i].reshape((args.width, args.width)) * 255).astype(np.uint8))
         mask_image.save('data/tmp_out/' + files[i])
 
+    times['crop'] = time.process_time()
     crop_images(files, [get_bounds(out[i]) for i in range(len(out))])
+    times['crop'] = time.process_time() - times['crop']
     pass
 
 def crop_images(files, bounds):
@@ -46,24 +52,34 @@ def crop_images(files, bounds):
         im.save(path)
     pass
 
+def find_subarray(arr, cutoff=0.1):
+    sum_arr = np.cumsum(arr)
+    sum_arr = sum_arr / sum_arr[-1]
+    for i in range(len(arr)):
+        if sum_arr[i] < cutoff:
+            left = i
+        if sum_arr[i] < 1 - cutoff:
+            right = i
+    return (left,right)
+            
 
-def get_bounds(heatmap):
+
+
+def get_bounds(heatmap, buffer=0.2):
     #left=top=0.1
     #right=bottom=0.9
     #TODO actually compute the bounds. This is the complicated part
     probs = np.array(heatmap)
-    col = np.mean(probs,axis=0).argmax() / args.width
-    row = np.mean(probs,axis=1).argmax() / args.width
+    cols = np.mean(probs,axis=0)#.argmax() / args.width
+    rows = np.mean(probs,axis=1)#.argmax() / args.width
     #Primitive way to select the values
-    right = col + 0.4
-    left = col - 0.4
-    bottom = row + 0.4
-    top = row - 0.4
+    left,right = find_subarray(cols)
+    top,bottom = find_subarray(rows)
     #Note, bounds must be returned as a number from 0 to 1, it is a percentage
-    top = np.clip(top, 0, 1)
-    bottom = np.clip(bottom,0,1)
-    left = np.clip(left,0,1)
-    right = np.clip(right,0,1)
+    top = np.clip(top/args.width - buffer, 0, 1)
+    bottom = np.clip(bottom/args.width + buffer,0,1)
+    left = np.clip(left/args.width - buffer,0,1)
+    right = np.clip(right/args.width + buffer,0,1)
     #clip
     return (left,top,right,bottom)
 
@@ -92,6 +108,7 @@ if __name__ == "__main__":
     parser.add_argument('--masks', default='data/masks', help='A folder where we will store the output masks for each input image')
     parser.add_argument('--tmp_folder', default='data/tmp', help='A folder we will use to tinker with temporary data')
     parser.add_argument('--width', default=32*4, help='The width and height of the images for processing')
+    times = {'heatmap':0, 'crop':0}
     #Parse the args
     args = parser.parse_args()
     #Run the main program
