38e06af37c93cbc4e960e2a336c185f0d5169c6b
Isidor Kaplan <isidor.kaplan@mail.utoronto.ca>
2021-01-26 15:07:08 -0500

    Adding in validation support for training

--- training.py
+++ training.py
@@ -34,15 +34,25 @@ def train():
         rescale=1. / 255,
         shear_range=0.2,
         zoom_range=0.2,
+        validation_split=args.val_split,
         horizontal_flip=True)
+    #Create the train generator
     train_image_generator = train_datagen.flow_from_directory(
         args.folder + '/frames',
         batch_size=args.batch_size, target_size=(args.width, args.width),class_mode=None, seed=1, subset='training')
     train_mask_generator = train_datagen.flow_from_directory(
         args.folder + '/masks',
         batch_size=args.batch_size, target_size=(args.width, args.width),class_mode=None, seed=1, subset='training')
-    #Merge to create the overall pair generator
     train_generator = zip(train_image_generator,train_mask_generator)
+    #Create the validation
+    val_image_generator = train_datagen.flow_from_directory(
+        args.folder + '/frames',
+        batch_size=args.batch_size, target_size=(args.width, args.width),class_mode=None, seed=1, subset='validation')
+    val_mask_generator = train_datagen.flow_from_directory(
+        args.folder + '/masks',
+        batch_size=args.batch_size, target_size=(args.width, args.width),class_mode=None, seed=1, subset='validation')
+    val_generator = zip(val_image_generator,val_mask_generator)
+    
 
     # define model
     model = Unet('resnet34', encoder_weights='imagenet')
@@ -54,7 +64,8 @@ def train():
     #Display the model pre-training
     model.summary()
     #Train the model and save it's history
-    history = model.fit(train_generator,steps_per_epoch=args.steps_per_epoch, epochs=args.epoch)
+    history = model.fit(train_generator,steps_per_epoch=args.steps_per_epoch, epochs=args.epoch, \
+    validation_data = val_generator, validation_steps = args.val_samples )
     #Save the model weights
     model.save_weights(args.net_file)
     #Print the history as a graph
@@ -65,18 +76,18 @@ def train():
 
 def plotHistory(history):
     # Plot history: MSE
-    plt.plot(history.history['iou_score'], label='Training IOU Score')
-    #plt.plot(history.history['val_iou_score'], label='Score (validation data)')
-    plt.title('Accuracy on segmenting insulators')
+    plt.plot(history.history['iou_score'], label='Training')
+    plt.plot(history.history['val_iou_score'], label='Validation')
+    plt.title('IOU Accuracy')
     plt.ylabel('IOU Score')
     plt.xlabel('No. epoch')
     plt.legend(loc="upper left")
     plt.show()
     
     # Plot history: MSE
-    plt.plot(history.history['loss'], label='Training Loss')
-    #plt.plot(history.history['val_loss'], label='Score (validation data)')
-    plt.title('Loss at segmenting insulators')
+    plt.plot(history.history['loss'], label='Training')
+    plt.plot(history.history['val_loss'], label='Validation')
+    plt.title('BCE Training Loss')
     plt.ylabel('Loss')
     plt.xlabel('No. epoch')
     plt.legend(loc="upper left")
@@ -138,10 +149,12 @@ if __name__ == "__main__":
     parser.add_argument('--folder', default='data/heatmap', help='Enter the folder for dataset training')
     #Neural network arguments
     parser.add_argument('--epoch', type=int, default=300, help='The number of epoches to run')
-    parser.add_argument('--steps_per_epoch', type=int, default=5, help='The number of training entries to be included in each epoch (note we artificially extend our dataset)')
-    parser.add_argument('--batch_size', type=int,default=3, help='Not really sure what this does')
+    parser.add_argument('--steps_per_epoch', type=int, default=5, help='The number of training steps to run during each epoch')
+    parser.add_argument('--batch_size', type=int,default=3, help='The number of images to be included per training step')
     parser.add_argument('--net_file', default='data/heatmap.h5', help='What is the file to store the resulting neural network')
     parser.add_argument('--lr', type=float,default=0.001, help='The learning rate for the neural network')
+    parser.add_argument('--val_split', type=float , default=0.2, help='The validation split for the dataset')
+    parser.add_argument('--val_samples', type=int , default=5, help='The number of validation samples to show each epoch')
     parser.add_argument('--width', type=int,default=32*4, help='The width and height of the images for processing')
     parser.add_argument('--load', default=False, action='store_true', help='Set flag to enable loading an existing neural network')
     #Misc args
